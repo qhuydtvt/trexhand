c89ea3eb9713eb779055b6b09229217694ff71e6
huynq <qhuydtvt@gmail.com>
2018-02-05 01:39:09 +0700

    Added jumping

--- trex/trex.py
+++ trex/trex.py
@@ -3,17 +3,36 @@ from bases.renderer import *
 from trex.state import TRexState
 from trex.animator import TRexAnimator
 from inputs import input
+from bases.vector2d import Vector2D
 
 class TRex(GameObject):
     def __init__(self):
         GameObject.__init__(self)
+        self.velocity = Vector2D(0, 0)
         self.renderer = TRexAnimator()
         self.state = TRexState.IDLE
+        self.jump_speed = 15
+        self.gravity = 2
+        self.base_y = 0
 
     def run(self):
         GameObject.run(self)
         self.renderer.update(self)
-        print(input.jump_pressed, input.duck_pressed)
+        self.move_vertical()
+
+    def move_vertical(self):
+        if input.jump_pressed and self.state != TRexState.JUMPING:
+            self.velocity.y = -self.jump_speed
+            self.state = TRexState.JUMPING
+        else:
+            self.velocity.y += self.gravity
+
+        if self.position.y + self.velocity.y >= self.base_y:
+            self.position.y = self.base_y
+            self.velocity.y = 0
+            self.state = TRexState.RUNNING
+        else:
+            self.position.y += self.velocity.y
 
     def set_initial_position(self, x, y):
         self.position.x = x
